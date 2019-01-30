f73ad858fa4966a14ecb5515606f7c7640964c0b
dubemc <maggie@thedubes.com>
2019-01-30 09:49:55 -0500

    Added cites

--- dataFrame_1.py
+++ dataFrame_1.py
@@ -5,3 +5,8 @@ data = pd.read_csv("JanuaryData.csv")
 
 #default n=5, so it will only print 5 rows of data
 data.head()
+
+
+"""Cites:
+  -https://www.shanelynn.ie/python-pandas-read_csv-load-data-from-csv-files/
+  """
